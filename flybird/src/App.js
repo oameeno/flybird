78e5d21ef94b434255c5d098361da42c0b083411
Amin Albatayneh <ameenmohammad@live.com>
2023-02-12 14:24:01 -0600

    created obstacles + initiated new obstacles with randomly generated heights to progress

--- flybird/src/App.js
+++ flybird/src/App.js
@@ -7,10 +7,16 @@ const GAME_WIDTH = 500;
 const GAME_HEIGHT = 500;
 const GRAVITY = 6;
 const JUMP_HEIGHT = 100;
+const OBSTACLE_WIDTH = 40;
+const OBSTACLE_GAP = 200;
 
 function App() {
   const [birdPosition, setBirdPosition] = useState(250);
   const [gameHasStarted, setGameHasStarted] = useState(false);
+  const [obstacleHeight, setObstacleHeight] = useState(100);
+  const [obstacleLeft, setObstacleLeft] = useState(GAME_WIDTH - OBSTACLE_WIDTH);
+  
+  const bottomObstacleHeight = GAME_HEIGHT - OBSTACLE_GAP - obstacleHeight;
 
 useEffect(() => {
   let timeId;
@@ -39,6 +45,20 @@ const handleClick = () => {
   return (
     <Div onClick={handleClick}>
       <GameBox height={GAME_HEIGHT} width={GAME_WIDTH}>
+        <Obstacle 
+        top={0}
+        width={OBSTACLE_WIDTH}
+        height={obstacleHeight}
+        left={obstacleLeft}
+        />
+
+        <Obstacle 
+        top={GAME_HEIGHT - (obstacleHeight + bottomObstacleHeight)}
+        width={OBSTACLE_WIDTH}
+        height={bottomObstacleHeight}
+        left={obstacleLeft}
+        />
+
       <Bird size={BIRD_SIZE} top={birdPosition} />
       </GameBox>
     </Div>
@@ -66,4 +86,13 @@ const GameBox = styled.div`
 height: ${(props) => props.height}px;
 width: ${(props) => props.width}px;
 background-color: blue;
+`;
+
+const Obstacle = styled.div`
+position: relative;
+top: ${(props) => props.top}px;
+background-color: green;
+width: ${(props) => props.width}px;
+height: ${(props) => props.height}px;
+left: ${(props) => props.left}px;
 `;
